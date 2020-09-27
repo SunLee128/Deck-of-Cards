34c2f0564fc9d9dc1e56e112d76c32f7a6bcce5f
SunLee128 <sunlee128@gmail.com>
2020-09-27 13:32:24 +1000

    Refactor using hooks

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react'
-import './Card.css'
+import React from 'react';
+import './Card.css';
 
-class Card extends Component {
-  constructor(props){
-    super(props)
-    let angle = Math.random() * 90 -45
-    let x = Math.random() * 40 -20
-    let y = Math.random() * 40 -20
-    this._transform = `translate(${x}px,${y}px) rotate(${angle}deg)`
-  }
-  render(){
-    return (
-      <img style={{transform: this._transform }} 
-      className="card" 
-      src={this.props.image} 
-      alt={this.props.name} />
-    )
-  }
+function Card ({ image, name }){
+  let angle = Math.random() * 90 - 45;
+  let x = Math.random() * 40 - 20;
+  let y = Math.random() * 40 - 20;
+  let _transform = `translate(${x}px,${y}px) rotate(${angle}deg)`;
+
+  return <img style={{ transform: _transform }} className="card" src={image} alt={name} />;
 }
-export default Card
+export default Card;
--- src/Deck.js
+++ src/Deck.js
@@ -1,61 +1,64 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Card from './Card'
-import './Deck.css'
-const API_BASE_URL = "https://deckofcardsapi.com/api/deck"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './Card';
+import './Deck.css';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
-class Deck extends Component {
-  constructor(props){
-    super(props)
-    this.state = {deck:null, drawn: [], remaining: 52}
-    this.getCard = this.getCard.bind(this)
-  }
+function Deck (){
+  const [ deck, setDeck ] = useState(null);
+  const [ drawn, setDrawn ] = useState([]);
+  const [ remaining, setRemaining ] = useState(52);
 
-  async componentDidMount() {
-    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`)
-    this.setState({ deck:deck.data})
-  }
+  // state = {deck:null, drawn: [], remaining: 52}
 
-  async getCard() {
-    let id = this.state.deck.deck_id
-    try {
-      let cardUrl = `${API_BASE_URL}/${id}/draw/`
-      let cardRes = await axios.get(cardUrl)
-      if (!cardRes.data.success){
-        throw new Error("No card remaning")
-      }
-      let card = cardRes.data.cards[0]
-      this.setState(st =>( {
-        drawn: [
-          ...st.drawn,
-          {
-            id: card.code,
-            image: card.image,
-            name: `${card.value} of ${card.suit}`,
-            
-          }
-        ]
-      }))
-      this.setState({remaining: cardRes.data.remaining})
-    } catch(err) {
-      alert(err)
+  // componentDidMount(){
+  //   let deck =  axios.get(`${API_BASE_URL}/new/shuffle/`)
+  //   setState({ deck:deck.data})
+  // }
+  useEffect(() => {
+    async function getDeck (){
+      let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+      setDeck(deck.data);
     }
-  }
+    getDeck();
+  }, []);
 
-  render() {
-    const cards = this.state.drawn.map(c => (
-      <Card key={c.id} name={c.name} image={c.image} remaining={c.remaining}/>
-    ))
-    return (
-      <div className="Deck">
-        <h1 className="Deck-title">CARD DEALER</h1>
-        <h2 className="Deck-title subtitle">{this.state.remaining} cards remanining </h2>
-        <button className="Deck-btn" onClick={this.getCard}>Get Card</button>
-        <div className="Deck-cardarea">
-          {cards}
-        </div>
-      </div>
-    )
-  }
+  const getCard = async () => {
+    let id = deck.deck_id;
+    try {
+      let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
+      if (!cardRes.data.success) {
+        throw new Error('No card remaning');
+      }
+      let card = cardRes.data.cards[0];
+      setDrawn([
+        ...drawn,
+        {
+          id: card.code,
+          image: card.image,
+          name: `${card.value} of ${card.suit}`,
+        },
+      ]);
+      setRemaining(cardRes.data.remaining);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const cards = drawn.map((c) => <Card key={c.id} name={c.name} image={c.image} remaining={c.remaining} />);
+  return (
+    <div className="Deck">
+      <h1 className="Deck-title">CARD DEALER</h1>
+      <h2 className="Deck-title subtitle">{remaining} cards remanining </h2>
+      <button className="Deck-btn" onClick={getCard}>
+        Get Card
+      </button>
+      {/* <button className="Deck-btn" onClick={setDrawn([])}>
+        Reset
+      </button> */}
+      <div className="Deck-cardarea">{cards}</div>
+    </div>
+  );
 }
-export default Deck
+export default Deck;
